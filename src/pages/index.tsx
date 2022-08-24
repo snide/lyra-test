9989dc7c1d2cdae71f04eb6345916456465b8e48
Dave Snider <dave.snider@gmail.com>
2022-08-23 21:17:44 -0400

    feat: lyra test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,47 @@ import Image from 'next/image';
 
 import styles from '@/styles/Home.module.css';
 
+import { create, insert, search } from '@lyrasearch/lyra';
+
+const movieDB = create({
+  schema: {
+    title: `string`,
+    director: `string`,
+    plot: `string`,
+    year: `number`,
+    isFavorite: `boolean`,
+  },
+});
+
+const { id: thePrestige } = insert(movieDB, {
+  title: `The prestige`,
+  director: `Christopher Nolan`,
+  plot: `Two friends and fellow magicians become bitter enemies after a sudden tragedy. As they devote themselves to this rivalry, they make sacrifices that bring them fame but with terrible consequences.`,
+  year: 2006,
+  isFavorite: true,
+});
+
+const { id: bigFish } = insert(movieDB, {
+  title: `Big Fish`,
+  director: `Tim Burton`,
+  plot: `Will Bloom returns home to care for his dying father, who had a penchant for telling unbelievable stories. After he passes away, Will tries to find out if his tales were really true.`,
+  year: 2004,
+  isFavorite: true,
+});
+
+const { id: harryPotter } = insert(movieDB, {
+  title: `Harry Potter and the Philosopher's Stone`,
+  director: `Chris Columbus`,
+  plot: `Harry Potter, an eleven-year-old orphan, discovers that he is a wizard and is invited to study at Hogwarts. Even as he escapes a dreary life and enters a world of magic, he finds trouble awaiting him.`,
+  year: 2001,
+  isFavorite: false,
+});
+
+const searchResult = search(movieDB, {
+  term: `Harry`,
+  properties: `*`,
+});
+
 export default function Home() {
   return (
     <div className={styles.container}>
